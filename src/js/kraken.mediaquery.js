876720e4a35362dfc4276e88ef6b384a8ae05069
James O'Toole <james.otoole@ubc.ca>
2025-01-20 15:53:29 -0800

    Clean up

--- src/js/kraken.mediaquery.js
+++ src/js/kraken.mediaquery.js
@@ -4,16 +4,17 @@
  * Desc: js for adding a mediaquery class to the body
  *
  */
-(function(Drupal) {
-    Drupal.behaviors.krakenMediaQuery = {
-        attach(context, settings) {
-            var query = window.matchMedia("(max-width: 979px)"),
-            body = document.querySelector("body");
-            function watch(query) {
-                query.matches ? body.setAttribute("data-screen", "sm") : body.setAttribute("data-screen", "lg")
-            }
-            watch(query),
-            query.addEventListener("change", watch)
-        }
-    };
+(function (Drupal) {
+  Drupal.behaviors.krakenMediaQuery = {
+    attach(context, settings) {
+      var query = window.matchMedia("(max-width: 979px)"),
+        body = document.querySelector("body");
+      function watch(query) {
+        query.matches
+          ? body.setAttribute("data-screen", "sm")
+          : body.setAttribute("data-screen", "lg");
+      }
+      watch(query), query.addEventListener("change", watch);
+    },
+  };
 })(Drupal);
